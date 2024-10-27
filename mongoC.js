43d64fbdb0bc9d82fa891a1f06e19980d121a933
MPAM DEV03 <mpamdev03@MPAMs-Air.true.true>
2024-10-27 22:34:07 +0700

    Update Mongodb connection string

--- mongoC.js
+++ mongoC.js
@@ -3,10 +3,7 @@ import { MongoClient } from "mongodb";
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
 
 // const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
-const connectionString_ = `mongodb+srv://bom2:${password}@cluster0.6vwxi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; // clustore url
-
-
-
+const connectionString = `mongodb+srv://bom2:${password}@cluster0.6vwxi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; // clustore url
 
 const client = new MongoClient(connectionString);
 let conn;
